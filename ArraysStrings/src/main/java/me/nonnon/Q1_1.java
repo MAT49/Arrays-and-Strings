5dd4ee6e82d496f601c30b971bd427a9e9687a19
Ha Hwang <spyderthing@gmail.com>
2020-06-17 17:44:09 -0400

    Added method: doesStringHaveUniqueCharacters

--- ArraysStrings/src/main/java/me/nonnon/Q1_1.java
+++ ArraysStrings/src/main/java/me/nonnon/Q1_1.java
@@ -4,6 +4,18 @@ public class Q1_1 {
 	// Implement an algorithm to determine if a string has all unique characters. What if you cannot 
 	// use additional data structures?
 	
-	
+	public boolean doesStringHaveUniqueCharacters(String str) {
+		String lowerStr = str.toLowerCase();
+		boolean[] flagArr = new boolean[128];
+		for (int i = 0; i < str.length(); i++) {
+			int val = lowerStr.charAt(i);
+			if (flagArr[val]) {
+				return false;
+			}
+			flagArr[val] = true;
+		}
+		
+		return true;
+	}
 	
 }
